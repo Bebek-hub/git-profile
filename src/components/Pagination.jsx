da074faa72db357415d74732addd2da57c92851b
God'sFavour Omoare <godsfavourtolu@gmail.com>
2023-08-08 18:03:18 +0100

    added pagination

--- src/components/Pagination.jsx
+++ src/components/Pagination.jsx
@@ -1,9 +1,20 @@
-// import React from 'react'
+import { useState } from "react";
 
-const Pagination = () => {
-  return (
-    <div>Pagination</div>
-  )
-}
+const Pagination = (repos) => {
+  const [currentPage, setCurrentPage] = useState(1);
 
-export default Pagination
+  const perPage = 10;
+  const total = repos?.length;
+  const pages = Math.ceil(total / perPage);
+  console.log(repos)
+
+
+  // Math.ceil is used to round up the numbers incase the division gives a decimal number while Math.floor runs down the number.
+
+  const skip = currentPage * perPage - perPage;
+  // for the first page, nothing is skipped. the second page, 5 is skipped which means the whole of first page. the third page, ten is skipped which means the whole of first and second page. and so on.
+
+  return <div>Pagination</div>;
+};
+
+export default Pagination;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,9 +5,9 @@ import ErrorPage from "../components/ErrorPage";
 
 const Home = () => {
   const [repos, setRepos] = useState([]);
-  const [errors, setErrors] = useState('');
+  const [errors, setErrors] = useState("");
   const [currentPage, setCurrentPage] = useState(1);
-  const [postsPerPage, setPostsPerPage] = useState(5);
+  // const [postsPerPage, setPostsPerPage] = useState(5);
 
   useEffect(() => {
     const fetchRepos = async () =>
@@ -16,21 +16,27 @@ const Home = () => {
         .then((data) => {
           setRepos(data);
         })
-    .catch((err) => {
-      setErrors(err.message)
-      console.log(`the error is: ${err.message}`)
-    });
+        .catch((err) => {
+          setErrors(err.message);
+          console.log(`the error is: ${err.message}`);
+        });
     fetchRepos();
   }, []);
 
-  if (errors) return <ErrorPage error={errors} />
+  if (errors) return <ErrorPage error={errors} />;
 
+  const perPage = 5;
   const total = repos?.length;
-  console.log(total)
-  const indexOfLastRepo = currentPage * postsPerPage;  
-  const indexOfFirstRepo = indexOfLastRepo - postsPerPage;
-  const currentRepos = repos.slice(indexOfFirstRepo, indexOfLastRepo);
-  console.log(currentRepos)
+  const pages = Math.ceil(total / perPage);
+
+  // Math.ceil is used to round up the numbers incase the division gives a decimal number while Math.floor runs down the number.
+
+  const skip = currentPage * perPage - perPage;
+  // for the first page, nothing is skipped. the second page, 5 is skipped which means the whole of first page. the third page, ten is skipped which means the whole of first and second page. and so on.
+
+  // const indexOfLastRepo = currentPage * postsPerPage;
+  // const indexOfFirstRepo = indexOfLastRepo - postsPerPage;
+  // const currentRepos = repos.slice(indexOfFirstRepo, indexOfLastRepo);
 
   return (
     <section>
@@ -43,9 +49,39 @@ const Home = () => {
       </header>
 
       <div className="repo_container">
-        {repos.map((repo) => (
+        {/* {repos.map((repo) => (
+          <EachRepo key={repo.id} repo={repo} />
+        ))} */}
+
+        {repos?.slice(skip, skip + perPage).map((repo) => (
           <EachRepo key={repo.id} repo={repo} />
         ))}
+
+        <button
+          disabled={currentPage <= 1}
+          onClick={() => setCurrentPage((prev) => prev - 1)}
+        >
+          Prev
+        </button>
+        <p className="pagination">
+          Pages: {currentPage} of {pages}
+        </p>
+        {
+          <button
+            disabled={currentPage >= pages}
+            aria-disabled={currentPage >= pages}
+            onClick={() => setCurrentPage((prev) => prev + 1)}
+          >
+            next
+          </button>
+        }
+        {/* another magic here */}
+        {Array.from({ length: pages }, (value, index) => index + 1).map(
+          (each) => (
+            // eslint-disable-next-line react/jsx-key
+            <button onClick={() => setCurrentPage(each)}>{each}</button>
+          )
+        )}
       </div>
     </section>
   );
